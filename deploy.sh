682f4c381293185fd891ea33dba8d65efa7a54c8
Guilherme Rodrigues <gadr90@gmail.com>
2013-03-11 00:34:21 -0300

    New deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,9 @@
 git checkout gh-pages
 git merge master
 wintersmith build
+cp -Rfp build/* ./
+rm -rf build
+git add --all .
 git commit -am "New build for deployment"
 git push origin gh-pages
 git checkout master
